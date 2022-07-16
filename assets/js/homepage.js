7d1ee390c344c875405b671da003a83292deaf66
devihall <devis0306@gmail.com>
2022-07-16 18:38:42 -0400

    connecting the files

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -58,9 +58,10 @@ var displayRepos = function (repos, searchTerm) {
     // format repo name
     var repoName = repos[i].owner.login + "/" + repos[i].name;
 
-    // create a container for each repo
-    var repoEl = document.createElement("div");
+    // create a link for each repo
+    var repoEl = document.createElement("a");
     repoEl.classList = "list-item flex-row justify-space-between align-center";
+    repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
 
     // create a span element to hold repository name
     var titleEl = document.createElement("span");
